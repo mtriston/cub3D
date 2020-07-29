c362337d844bc89f61683a0e8d8ec9df25637030
Alexander Spivak <po4ta.polevaya@gmail.com>
2020-07-29 21:14:40 +0300

    add vars struct, include libs and functions' prototypes

---
+++ cub3D.h
@@ -0,0 +1,54 @@
+#ifndef CUB3D_H
+# define CUB3D_H
+
+#define WINDOW_WIDTH 500
+#define WINDOW_HEIGHT 500
+#define TILE_SIZE 32 
+
+#define MOVE_SPEED 4 
+#define ROTATION_SPEED 2 * 0.017453293 
+
+#define	PI 3.14159265359
+#define FOV_ANGLE 1.047197551
+
+#define WALL_STRIP_WIDTH 1
+#define NUM_RAYS WINDOW_WIDTH / WALL_STRIP_WIDTH
+
+#define LEFT_ARROW 65361
+#define UP_ARROW 65362
+#define RIGHT_ARROW 65363
+#define DOWN_ARROW 65364
+
+#include <mlx.h>
+#include <math.h>
+#include <fcntl.h>
+#include "ft_printf/ft_printf.h"
+#include "get_next_line/get_next_line.h"
+
+typedef struct	s_vars
+{
+	void		*mlx;
+	void		*win;
+	int			x;
+	int			y;
+	double		turn_direction;
+	int			walk_direction;
+	double		rotation_angle;
+	void		*img;
+	char		*addr;
+	int			bits_per_pixel;
+	int			line_length;
+	int			endian;
+	int			**map;
+}				t_vars;
+
+int	**parse_map(char *path);
+void	draw_minimap(t_vars *vars);
+void	ft_rect(int x, int y, int width, int height, int color, t_vars *vars);
+int	create_trgb(int t, int r, int g, int b);
+int	get_t(int trgb);
+int	get_r(int trgb);
+int	get_g(int trgb);
+int	get_b(int trgb);
+
+#endif
