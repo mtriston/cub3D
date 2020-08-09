7809e89490555f87f599c4bd80e8418a4fa9d24a
Alexander Spivak <po4ta.polevaya@gmail.com>
2020-08-09 15:01:14 +0300

    func which read texure from file

---
+++ src/texture.c
@@ -0,0 +1,9 @@
+#include "../cub3D.h"
+
+void	import_texture(char *path, t_img *texture, void *mlx)
+{
+	//TODO: добавить обработку ошибок
+	texture->img = mlx_xpm_file_to_image(mlx, path, &texture->width, &texture->height);
+	if (texture->img)
+		texture->addr = mlx_get_data_addr(&texture->img, &texture->bits_per_pixel, &texture->line_length, &texture->endian);
+}
