f2278044ce912c6328703502cd499132e4e20c81
Alexander Spivak <po4ta.polevaya@gmail.com>
2020-08-22 13:04:14 +0300

    rewrite bitmap creating. now it works correctly

--- src/bitmap.c
+++ src/bitmap.c
@@ -11,54 +11,50 @@
 /* ************************************************************************** */
 
 #include "../cub3D.h"
+#include "../bitmap.h"
 
-void    file_header(int fd, t_vars *vars)
+t_bmp_file_header   file_header(t_vars *vars)
 {
-	unsigned char   bfh[14];
-	int             size;
+	t_bmp_file_header bfh;
 
-	ft_bzero(bfh, 14);
-	bfh[0] = 'B';
-	bfh[1] = 'M';
-	size = 14 + 40 + (vars->screen.width * vars->screen.height * 4);
-	bfh[2] = size % 256;
-	bfh[3] = size / 256 % 256;
-	bfh[4] = size / 256 / 256 % 256;
-	bfh[5] = size / 256 / 256 / 256;
-	bfh[10] = 14 + 40;
-	if (!write(fd, bfh, 14))
-		ft_exit("file header error", vars);
+	bfh.type = 0x4D42;
+	bfh.size = sizeof(t_bmp_file_header) + sizeof(t_bmp_info_header) + \
+				vars->screen.width * vars->screen.height * 4;
+	bfh.reserved1 = 0;
+	bfh.reserved2 = 0;
+	bfh.off_bits = sizeof(t_bmp_file_header) + sizeof(t_bmp_info_header);
+	return (bfh);
 }
 
-void    info_header(int fd, t_vars *vars)
+t_bmp_info_header   info_header(t_vars *vars)
 {
-	unsigned char   bih[40];
-	int32_t         size;
-
-	ft_bzero(bih, 40);
-	bih[0] = 40;
-	size = vars->screen.width;
-	bih[4] = size % 256;
-	bih[5] = size / 256 % 256;
-	bih[6] = size / 256 / 256 % 256;
-	bih[7] = size / 256 / 256 / 256;
-	size = vars->screen.height;
-	bih[8] = size % 256;
-	bih[9] = size / 256 % 256;
-	bih[10] = size / 256 / 256 % 256;
-	bih[11] = size / 256 / 256 / 256;
-	bih[12] = 1;
-	bih[14] = vars->img.bits_per_pixel;
-	if (!write(fd, bih, 40))
-		ft_exit("header info error", vars);
+	t_bmp_info_header bih;
+	bih.size = 40;
+	bih.width = vars->screen.width;
+	bih.height = vars->screen.height * (-1);
+	bih.planes = 1;
+	bih.bit_count = vars->img.bits_per_pixel;
+	bih.compression = 0;
+	bih.size_image = 0;
+	bih.x_pels_per_meter = 0;
+	bih.y_pels_per_meter = 0;
+	bih.clr_used = 0;
+	bih.clr_important = 0;
+	return (bih);
 }
 void bitmap(t_vars *vars) {
 
 	int fd;
+	t_bmp_file_header bfh;
+	t_bmp_info_header bih;
 
 	fd = open("screenshot.bmp", O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU);
-	file_header(fd, vars);
-	info_header(fd, vars);
+	bfh = file_header(vars);
+	bih = info_header(vars);
+	if (!write(fd, &bfh, sizeof(bfh)))
+		ft_exit("file header error", vars);
+	if (!write(fd, &bih, sizeof(bih)))
+		ft_exit("info header error", vars);
 	if (!write(fd, vars->img.addr, vars->screen.width * vars->screen.height * 4))
 		ft_exit("bitmap data error", vars);
 	close(fd);
