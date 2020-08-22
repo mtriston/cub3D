8f4fcbfa56ef1ff5929090bd118612256d05e0c5
Alexander Spivak <po4ta.polevaya@gmail.com>
2020-08-22 22:24:49 +0300

    code style fix

--- src/setup.c
+++ src/setup.c
@@ -1,44 +1,70 @@
-#include "../cub3D.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   setup.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtriston <mtriston@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/22 22:18:48 by mtriston          #+#    #+#             */
+/*   Updated: 2020/08/22 22:19:23 by mtriston         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-static void	prepare_structs(t_vars *vars)
+#include "../includes/cub3D.h"
+
+static void	prepare_structs(t_cub *cub)
 {
-	vars->player.x = 0;
-	vars->player.y = 0;
-	vars->player.turn_direction = 0;
-	vars->player.walk_direction = 0;
-	vars->player.sideways = FALSE;
-	vars->player.rotation_angle = -1;
-	vars->player.walk_speed = 5;
-	vars->player.turn_speed = 5 * (M_PI / 180);
-	vars->screen.width = 0;
-	vars->screen.height = 0;
-	vars->map.map = NULL;
-	vars->map.floor_color = 0;
-	vars->map.ceil_color = 0;
-	vars->map.sprites = 0;
+	cub->frame.win = NULL;
+	cub->frame.mlx = NULL;
+	cub->texture.east.img = NULL;
+	cub->texture.west.img = NULL;
+	cub->texture.north.img = NULL;
+	cub->texture.south.img = NULL;
+	cub->texture.sprite.img = NULL;
+	cub->img.img = NULL;
+	cub->cam.x = 0;
+	cub->cam.y = 0;
+	cub->cam.turn_dir = 0;
+	cub->cam.walk_dir = 0;
+	cub->cam.sideways = FALSE;
+	cub->cam.angle = -1;
+	cub->cam.walk_speed = 5;
+	cub->cam.turn_speed = 5 * (M_PI / 180);
+	cub->frame.w = 0;
+	cub->frame.h = 0;
+	cub->map.map = NULL;
+	cub->map.f_clr = 0;
+	cub->map.c_clr = 0;
+	cub->map.items = 0;
 }
 
-static void	init_img(t_vars *vars, t_img *img, t_screen *screen)
+static void	init_img(t_img *img, t_frame *screen, t_cub *cub)
 {
-	screen->win = \
-			mlx_new_window(screen->mlx, screen->width, screen->height, "cub3D");
-	if (screen->win == NULL)
-		ft_exit("mlx_new_window failed", vars);
-	img->img = mlx_new_image(screen->mlx, screen->width, screen->height);
+	img->img = mlx_new_image(screen->mlx, screen->w, screen->h);
 	if (img->img == NULL)
-		ft_exit("mlx_new_image failed", vars);
-	img->addr = mlx_get_data_addr(img->img, &img->bits_per_pixel, \
-									&img->line_length, &img->endian);
+		ft_exit("mlx_new_image failed", cub);
+	img->addr = mlx_get_data_addr(img->img, &img->bpp, \
+									&img->line_len, &img->endian);
 	if (img->addr == NULL)
-		ft_exit("mlx_get_data_addr failed", vars);
+		ft_exit("mlx_get_data_addr failed", cub);
 }
 
-void		setup(t_vars *vars, char *path)
+void		setup(t_cub *cub, char *path, int bmp)
 {
-	vars->screen.mlx = mlx_init();
-	if (vars->screen.mlx == NULL)
-		ft_exit("MLH initialization failed", vars);
-	prepare_structs(vars);
-	parser(path, vars);
-	init_img(vars, &vars->img, &vars->screen);
+	prepare_structs(cub);
+	cub->frame.mlx = mlx_init();
+	if (cub->frame.mlx == NULL)
+		ft_exit("MLH initialization failed", cub);
+	parser(path, cub);
+	if (!(cub->ray = malloc_gc(sizeof(t_ray) * cub->frame.w)))
+		ft_exit("Memory allocation for array of rays failed.", cub);
+	init_img(&cub->img, &cub->frame, cub);
+	render_next_frame(cub);
+	if (bmp == TRUE)
+		bitmap(cub);
+	cub->frame.win = \
+			mlx_new_window(cub->frame.mlx, cub->frame.w, cub->frame.h, "cub3D");
+	if (cub->frame.win == NULL)
+		ft_exit("mlx_new_window failed", cub);
+	mlx_put_image_to_window(cub->frame.mlx, cub->frame.win, cub->img.img, 0, 0);
 }
