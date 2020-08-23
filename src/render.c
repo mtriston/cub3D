422eb6eb2bf205440a3e4c62998d808fb66e3ed5
Alexander Spivak <po4ta.polevaya@gmail.com>
2020-08-23 19:24:24 +0300

    refactoring and code style fix

--- src/render.c
+++ src/render.c
@@ -1,60 +1,76 @@
-#include "../cub3D.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   render.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtriston <mtriston@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/23 19:07:30 by mtriston          #+#    #+#             */
+/*   Updated: 2020/08/23 19:49:04 by mtriston         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-static t_img detect_side(t_vars *vars, t_ray ray)
+#include "../includes/cub3D.h"
+
+static t_img			detect_side(t_cub *cub, t_ray ray)
 {
-	if (ray->is_vertical_hit)
+	if (ray.is_vert_hit)
 	{
-		if (ray->is_right)
-			return (vars->texture.east);
+		if (ray.is_right)
+			return (cub->texture.east);
 		else
-			return (vars->texture.west);
+			return (cub->texture.west);
 	}
 	else
 	{
-		if (ray->is_up)
-			return (vars->texture.south);
+		if (ray.is_up)
+			return (cub->texture.south);
 		else
-			return (vars->texture.north);
+			return (cub->texture.north);
 	}
 }
 
-void render_walls(t_vars *vars)
+static t_render_utils	calc_one_stripe(t_cub *cub, int i, t_img tex)
 {
-	int i;
-	double ray_distance;
-	double plane;
-	double wall_height;
-	double wall_top;
-	double step;
-	double tex_pos;
-	int texX;
-	t_img texture;
+	t_render_utils col;
+
+	col.plane = (cub->frame.w / 2.0) / tan(FOV / 2);
+	cub->ray[i].dist = cub->ray[i].dist == 0 ? 1 : cub->ray[i].dist;
+	col.dist = cub->ray[i].dist * cos(cub->ray[i].angle - cub->cam.angle);
+	col.height = (cub->map.tile / col.dist) * col.plane;
+	col.top = cub->frame.h / 2.0 - col.height / 2;
+	if (cub->ray[i].is_vert_hit)
+		col.t_x = remainder(cub->ray[i].hit_y, cub->map.tile) / \
+												cub->map.tile * tex.width;
+	else
+		col.t_x = remainder(cub->ray[i].hit_x, cub->map.tile) / \
+												cub->map.tile * tex.width;
+	col.t_x = col.t_x & (unsigned int)(tex.height - 1);
+	col.step = tex.height / col.height;
+	col.tex_pos = (col.top - cub->frame.h / 2.0 + col.height / 2.0) * col.step;
+	col.y = (int)col.top;
+	return (col);
+}
+
+void					render_walls(t_cub *cub)
+{
+	t_img			tex;
+	t_render_utils	c;
+	int				i;
 
 	i = 0;
-	plane = (vars->screen.width / 2) / tan(FOV_ANGLE / 2);
-	while (i < vars->screen.width)
+	while (i < cub->frame.w)
 	{
-		texture = detect_side(vars, &vars->ray[i]);
-		if (vars->ray[i].distance == 0)
-			vars->ray[i].distance = 1;
-		ray_distance = vars->ray[i].distance * cos(vars->ray[i].ray_angle - vars->player.rotation_angle);
-		wall_height = (vars->map.tile_size / ray_distance) * plane;
-		wall_top = vars->screen.height / 2 - wall_height / 2;
-
-		if (vars->ray[i].is_vertical_hit)
-			texX = remainder(vars->ray[i].hit_y, vars->map.tile_size) / vars->map.tile_size * texture.width;
-		else
-			texX = remainder(vars->ray[i].hit_x, vars->map.tile_size) / vars->map.tile_size * texture.width;
-		texX = texX & (texture.height - 1);
-		step = 1.0 * texture.height / wall_height;
-		tex_pos = (wall_top - vars->screen.height / 2 + wall_height / 2) * step;
-		for (int y = wall_top; y < wall_top + wall_height; y++)
+		tex = detect_side(cub, cub->ray[i]);
+		c = calc_one_stripe(cub, i, tex);
+		while (c.y < c.top + c.height)
 		{
-			int texY = (int)tex_pos & (texture.height - 1);
-			char *color = texture.addr + (texY * texture.line_length + texX * (texture.bits_per_pixel / 8));
-			tex_pos += step;
-			if (y >= 0 && y <= vars->screen.height && i >= 0 && i <= vars->screen.width)
-				my_mlx_pixel_put(&vars->img, i, y, *(unsigned int *)color);
+			c.t_y = (unsigned int)c.tex_pos & ((unsigned int)tex.height - 1);
+			c.color = tex.addr + (c.t_y * tex.line_len + c.t_x * (tex.bpp / 8));
+			c.tex_pos += c.step;
+			if (c.y >= 0 && c.y <= cub->frame.h && i >= 0 && i <= cub->frame.w)
+				my_mlx_pixel_put(&cub->img, i, c.y, *(unsigned int *)c.color);
+			c.y++;
 		}
 		i++;
 	}
