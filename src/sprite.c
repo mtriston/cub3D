8f8eafc0f31c3e245734e5497db586efe20f022e
Alexander Spivak <po4ta.polevaya@gmail.com>
2020-08-23 20:41:01 +0300

    refactoring and code  style

--- src/sprite.c
+++ src/sprite.c
@@ -5,102 +5,103 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mtriston <mtriston@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/08/16 16:13:48 by mtriston          #+#    #+#             */
-/*   Updated: 2020/08/16 20:57:30 by mtriston         ###   ########.fr       */
+/*   Created: 2020/08/23 20:15:16 by mtriston          #+#    #+#             */
+/*   Updated: 2020/08/23 20:39:40 by mtriston         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../cub3D.h"
+#include "../includes/cub3D.h"
 
-static void     calc_sprite_parameters(t_vars *vars)
+static void	calc_sprite_parameters(t_cub *cub)
 {
-	int i;
+	int	i;
 
-    i = 0;
-    while (i < vars->map.sprites)
-    {
-        vars->sprite[i].angle = atan2(vars->sprite[i].y - vars->player.y, \
-                                        vars->sprite[i].x - vars->player.x);
-        while (vars->sprite[i].angle - vars->player.rotation_angle > M_PI)
-		vars->sprite[i].angle -= 2.0 * M_PI;
-	    while (vars->sprite[i].angle - vars->player.rotation_angle < -M_PI)
-		vars->sprite[i].angle += 2.0 * M_PI;
-        vars->sprite[i].angle -= vars->player.rotation_angle;
-        vars->sprite[i].dist = sqrt(pow(vars->sprite[i].x - vars->player.x, 2)\
-                                + pow(vars->sprite[i].y - vars->player.y, 2));
-        vars->sprite[i].size = (vars->map.tile_size / vars->sprite[i].dist) *\
-                                (vars->screen.width / 2) / tan(FOV_ANGLE / 2);
-        if (vars->sprite[i].angle < FOV_ANGLE / 2 && \
-                                        vars->sprite[i].angle > -FOV_ANGLE / 2)
-            vars->sprite[i].hidden = TRUE;
-        i++;
-    }
-}
-
-static void sort_sprite(t_vars *vars)
-{
-    int i;
-    int j;
-    t_sprite temp;
-    
-    i = 0;
-    while (i < vars->map.sprites)
-    {
-        j = 0;
-        while (j < vars->map.sprites - 1)
-        {
-            if (vars->sprite[j].dist < vars->sprite[j + i].dist)
-            {
-                temp = vars->sprite[j];
-                vars->sprite[j] = vars->sprite[j + 1];
-                vars->sprite[j + 1] = temp;
-            }
-            j++;
-        }
-        i++;
-    }
-    
-}
-
-static void render_sprite(t_vars *vars)
-{
-	int		k;
-	double	x_offset;
-	double	y_offset;
-
-	k = 0;
-	while (k < vars->map.sprites)
+	i = 0;
+	while (i < cub->map.items)
 	{
-		x_offset = vars->screen.width / (FOV_ANGLE * (180 / M_PI)) * ((180 / M_PI * vars->sprite[k].angle) + 30) - vars->sprite[k].size / 2;
-		y_offset = vars->screen.height / 2 - vars->sprite[k].size / 2;
-		double step = 1.0 * vars->texture.sprite.height / vars->sprite[k].size;
-		for (int i = 0; i < vars->sprite[k].size; i++)
-		{	
-			if (x_offset + i > vars->screen.width || x_offset + i < 0)
-				continue;
-			if (vars->ray[i + (int)x_offset].distance < vars->sprite[k].dist)
-				continue;
-			int tex_x = i / vars->sprite[k].size * vars->texture.sprite.width;
-			double tex_pos = 0;
-			for (int j = 0; j < vars->sprite[k].size; j++)
-			{	
-				if (y_offset + j > vars->screen.height || y_offset + i < 0)
-				continue;
-				int tex_y = (unsigned int)tex_pos & (vars->texture.sprite.height - 1);
-				char *color = vars->texture.sprite.addr + (tex_y * vars->texture.sprite.line_length + tex_x * (vars->texture.sprite.bits_per_pixel / 8));
-				tex_pos += step;
-				if (*(unsigned int *)color == 0x00000000)
-					continue;
-				my_mlx_pixel_put(&vars->img, x_offset + i, y_offset + j, *(unsigned int *)color);
-			}
-		}
-        k++;
+		cub->item[i].angle = atan2(cub->item[i].y - cub->cam.y, \
+										cub->item[i].x - cub->cam.x);
+		while (cub->item[i].angle - cub->cam.angle > M_PI)
+			cub->item[i].angle -= 2.0 * M_PI;
+		while (cub->item[i].angle - cub->cam.angle < -M_PI)
+			cub->item[i].angle += 2.0 * M_PI;
+		cub->item[i].angle -= cub->cam.angle;
+		cub->item[i].dist = sqrt(pow(cub->item[i].x - cub->cam.x, 2) + \
+										pow(cub->item[i].y - cub->cam.y, 2));
+		cub->item[i].size = (cub->map.tile / cub->item[i].dist) * \
+										(cub->frame.w / 2) / tan(FOV / 2);
+		cub->item[i].x_ff = cub->frame.w / (FOV * (180 / M_PI)) * \
+			((180 / M_PI * cub->item[i].angle) + 30) - cub->item[i].size / 2;
+		cub->item[i].y_ff = cub->frame.h / 2 - cub->item[i].size / 2;
+		cub->item[i].step = cub->tex.item.height / cub->item[i].size;
+		cub->item[i].tex_pos = 0;
+		i++;
 	}
 }
 
-void        sprite(t_vars *vars)
+static void	sort_sprite(t_cub *cub)
 {
-    calc_sprite_parameters(vars);
-    sort_sprite(vars);
-    render_sprite(vars);
+	int		i;
+	int		j;
+	t_item	temp;
+
+	i = 0;
+	while (i < cub->map.items)
+	{
+		j = 0;
+		while (j < cub->map.items - 1)
+		{
+			if (cub->item[j].dist < cub->item[j + 1].dist)
+			{
+				temp = cub->item[j];
+				cub->item[j] = cub->item[j + 1];
+				cub->item[j + 1] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+static void	draw_sprite(t_cub *cub, t_item n)
+{
+	int				i;
+	int				j;
+	int				tex_x;
+	int				tex_y;
+
+	i = -1;
+	while (++i < n.size)
+	{
+		if (n.x_ff + i > cub->frame.w || n.x_ff + i < 0 || \
+							cub->ray[i + (int)n.x_ff].dist < n.dist)
+			continue;
+		tex_x = i / n.size * cub->tex.item.width;
+		n.tex_pos = 0;
+		j = -1;
+		while (++j < n.size)
+		{
+			tex_y = (unsigned int)n.tex_pos & (cub->tex.item.height - 1);
+			n.clr = cub->tex.item.addr + \
+				(tex_y * cub->tex.item.len + tex_x * (cub->tex.item.bpp / 8));
+			n.tex_pos += n.step;
+			if (n.y_ff + j > cub->frame.h || n.y_ff + i < 0 || *n.clr == 0x0)
+				continue;
+			my_mlx_pixel_put(&cub->img, n.x_ff + i, n.y_ff + j, *n.clr);
+		}
+	}
+}
+
+void		render_sprite(t_cub *cub)
+{
+	int	i;
+
+	i = 0;
+	calc_sprite_parameters(cub);
+	sort_sprite(cub);
+	while (i < cub->map.items)
+	{
+		draw_sprite(cub, cub->item[i]);
+		i++;
+	}
 }
